1214e2f64f22f997ad4b4adaeb957254edb11596
JosefLitos <invisiblemancz@gmail.com>
2021-05-11 13:53:48 +0200

    Changed white

--- config.h
+++ config.h
@@ -117,7 +117,7 @@ float alpha = 0.8;
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
 	"#101010", "#c82a15", "#669915", "#d8af15", "#1a6ab8", "#9d2088", "#45ac87", "#868380",
-	"#4a4846", "#f03522", "#85f022", "#f0e522", "#2798f0", "#d032c5", "#75f0aa", "#f0e8e0",
+	"#4a4846", "#f03522", "#85f022", "#f0e522", "#2798f0", "#d032c5", "#75f0aa", "#f0eee0",
 	[255] = 0,
 	/* more colors can be added after 255 to use with DefaultXX */
 	"#86e000", /* 256 -> cursor */

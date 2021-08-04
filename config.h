d199f6bfcbecad512574961a0b6b6d57b68cb5d0
JosefLitos <invisiblemancz@gmail.com>
2021-08-04 08:21:47 +0200

    changed line spacing

--- config.h
+++ config.h
@@ -30,7 +30,7 @@ char *vtiden = "\033[?6c";
 
 /* Kerning / character bounding-box multipliers */
 static float cwscale = 1;
-static float chscale = 0.9;
+static float chscale = 1;
 
 /*
  * word delimiter string

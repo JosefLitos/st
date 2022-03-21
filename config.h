83edf6eb4cbb7855cd02f08b14e3f08d9acafd84
JosefLitos <invisiblemancz@gmail.com>
2022-03-21 16:33:14 +0100

    fixed pasting text

--- config.h
+++ config.h
@@ -196,7 +196,7 @@ static MouseShortcut mshortcuts[] = {
 	{ XK_ANY_MOD,           Button5, ttysend,        {.s = "\005"} },
 	{ ShiftMask,            Button4, ttysend,        {.s = "\033[5;2~"} },
 	{ ShiftMask,            Button5, ttysend,        {.s = "\033[6;2~"} },
-	{ XK_ANY_MOD,           Button2, selpaste,       {.i = 0},      1 },
+	{ XK_ANY_MOD,           Button2, clippaste,       {.i = 0},      1 },
   { ControlMask,          Button1, externalpipe,   {.v = openurlcmd } },
 };
 
